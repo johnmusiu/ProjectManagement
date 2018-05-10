05a351578473749fb0eef7b2d921f4049fb8b8a6
johnmusiu <johnmusiu@gmail.com>
2018-05-11 02:11:14 +0300

    create initial migrations for DB design

---
+++ database/migrations/2018_05_10_165032_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->string('name');
+            $table->string('description');
+            $table->date('due_date');
+            $table->enum('status', ['open', 'ongoing', 'closed']);
+            $table->enum('priority', ['high', 'medium','low']);
+            $table->enum('access', ['public', 'private']);
+            $table->string('doc_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
