f437700f208aaa3fb798ab27f9c673b3176be081
johnmusiu <johnmusiu@gmail.com>
2018-05-12 19:59:43 +0300

    refactor: add department name to dashboard home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="panel panel-default">
+    <div class="panel-heading"><h3>Dashboard </h3><h4>{{ Auth::User()->department->name }} Department</h4></div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if(Auth::user()->hasRole('department_manager'))
-                        You are logged in as a department manager.
-                    @else
-                        You are logged in a junior employee.
-                    @endif
-                </div>
-            </div>
-        </div>
+    <div class="panel-body">
+        
+        <!-- table to hold tasks -->
+        @include('tasks.show')
+        <!-- end table -->
     </div>
 </div>
 @endsection
