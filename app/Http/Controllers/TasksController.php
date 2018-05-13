329d1fee65378e1b8387ce1b93df418ba44a80a6
johnmusiu <johnmusiu@gmail.com>
2018-05-13 05:53:57 +0300

    Add functionality for task: 1. creation: -save notification for user and department -save user task assignments 2. view task details: method to return selected task details to view 3. following: method to add a follow task relationship for user 4. mark-ongoing: method to update task as ongoing

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -9,6 +9,7 @@ use App\Task;
 use App\User;
 use App\Category;
 use App\Department;
+use App\Notification;
 
 class TasksController extends Controller
 {
@@ -44,6 +45,7 @@ class TasksController extends Controller
      */
     public function create(Request $request)
     {
+        // dd($request->all());
         $validator = Validator::make($request->all(), [
             'name' => 'required|min:2|max:255',
             'description' => 'required',
@@ -74,23 +76,30 @@ class TasksController extends Controller
         $task->user()->associate(\Auth::User()->id);
         $task->save();
 
-        foreach($request['department'] as $department_id){
-            DB::table('task_assignment')->insert([
-                'task_id' => $task->id, 
-                'department_id' => $department_id,
-                'assigned_to' => 'department',
-            ]);
+        // users to be notified
+        foreach($request['notify_users'] as $user_id){
+            $notification = new Notification();
+            $notification->task_id = $task->id;
+            $notification->user_id = $user_id;
+            $notification->save();
         }
 
-        foreach($request['users'] as $user_id){
-            DB::table('task_assignment')->insert([
-                'task_id' => $task->id, 
-                'user_id' => $user_id,
-                'assigned_to' => 'member',
-            ]);
+        // departments to be notified
+        foreach($request['department'] as $department_id){
+            $notification = new Notification();
+            $notification->task_id = $task->id;
+            $notification->department_id = $department_id;
+            $notification->save();
         }
+
+        // users assigned to perform a certain task
+        foreach($request['users'] as $user_id){
+            $task->users()->syncWithoutDetaching($user_id);
+        }
+
         $request = $request->all();
-        if($request['doc_name']){
+        // save file and add file name to db
+        if(isset($request['doc_name'])){
             foreach ($request['doc_name'] as $key => $value) {
                 $file_name = time(). str_replace(' ', '', $task->name) . '.' . 
                     $value->getClientOriginalExtension();
@@ -106,8 +115,55 @@ class TasksController extends Controller
         return redirect(route('create_task'));
     }
 
-    public function update()
+    /**
+     * method for editing a certain task
+     * @param Task $task
+     * @return redirect route
+     */
+    public function edit(Task $task)
     {
+        //update task
+        //call create or update?
         return true;
+    }
+
+    /**
+     * get and display task details
+     * @param Task $task
+     * @return view
+     */
+    public function view_details(Task $task)
+    {   
+        return view('tasks.view_details', compact('task'));
+    }
+
+    /**
+     * a method to add user follower to task
+     * @param Task $task
+     * @return redirect back
+     */
+    public function follow(Task $task)
+    {
+        //add follow => user to get notifications on task
+        $notification = new Notification();
+        $notification->task_id = $task->id;
+        $notification->user_id = $user_id;
+        $notification->save();
+
+        if($notification) session()->flash("message", "Task followed successfully");
+        else session()->flash("error", "Task follow failed");
+        return redirect(back());
+    }
+
+    /**
+     * mark task from open to ongoing
+     */
+    public function mark_ongoing(Task $task)
+    {
+        $task->status = 'ongoing';
+        if($task->save()) session()->flash("message", "Task marked as ongoing.");
+        else session()->flash("error", "Failed: Task not marked as ongoing");
+
+        return back();
     }
 }
