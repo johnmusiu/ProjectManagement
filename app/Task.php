54b15b783b29f61cdca18eb86c254a852cb43bba
johnmusiu <johnmusiu@gmail.com>
2018-05-13 05:44:33 +0300

    Add relationships btn task: 1. departments to be notified for task 2. users assigned 3. progress entries for task 4. latest progress entry

--- app/Task.php
+++ app/Task.php
@@ -4,6 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\Progress;
+use App\Category;
+use App\Department;
+
 
 class Task extends Model
 {
@@ -24,4 +28,37 @@ class Task extends Model
         return $this->belongsTo(Category::class);
     }
     
+
+    /**
+     * a task can be assigned to many users
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'task_user');
+    }
+
+    /**
+     *  a task can have many progress entries
+     */
+    public function progresses()
+    {
+        return $this->hasMany(Progress::class);
+    }
+
+    /**
+     * return the latest progress update on a post
+     */
+    public function latest_progress()
+    {
+        return $this->hasOne(Progress::class)->latest();
+    }
+
+    /**
+     * many departments can be notified on many particular tasks
+     * gets the departments listed for notifications for tasks
+     */
+    public function departments()
+    {
+        return $this->belongsToMany(Department::class, 'notifications');
+    }
 }
