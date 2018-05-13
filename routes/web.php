c7ada042a476e981e907b9cd91cf14a35bc5dc2a
johnmusiu <johnmusiu@gmail.com>
2018-05-13 05:54:16 +0300

    Add relevant routes

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/home/mytasks', 'HomeController@usertasks')->name('my_tasks');
 
 Route::get('/task/create', 'TasksController@index')->name('create_task');
 Route::post('/task/create', 'TasksController@create')->name('save_task');
+Route::get('/task/{task}', 'TasksController@view_details')->name('view_task');
+Route::post('/task/{task}', 'TasksController@edit')->name('edit_task');
+Route::post('/task/mark-ongoing/{task}', 'TasksController@mark_ongoing')->name('mark_ongoing');
+Route::post('/task/follow/{task}', 'TasksController@follow')->name('follow_task');
 
 Route::get('/category/create', 'CategoriesController@index')->name('create_category');
 Route::post('/category/create', 'CategoriesController@create')->name('save_category');
