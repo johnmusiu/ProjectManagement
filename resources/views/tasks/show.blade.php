be8ff88667be5c49f07b557a7f2243e6ea29a08c
johnmusiu <johnmusiu@gmail.com>
2018-05-15 08:59:21 +0300

    move tr to subtemplate .. clean the show template a bit

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -14,47 +14,15 @@
 </thead>
 <tbody>
     @foreach($tasks as $task)
-    <tr>
-        <td>{{ $task['id'] }}</td>
-        <td>{{ $task['name'] }}</td>
-        <td>{{ $task['access'] }}</td>
-        <td>{{ $task['priority'] }}</td>
-        <td>{{ Carbon\Carbon::parse($task['due_date'])->diffForHumans() }}</td>
-        <td>{{ $task['status'] }}</td>
-        @if($task->latest_progress != null)
-        {{ dd($task->latest_progress)}}
-            <td>
-                {{ $task->latest_progress->progress }}%
-            </td>
-            <td>
-                {{ $task->latest_progress->message }}
-            </td>
-        @else
-            <td>0%</td>
-            <td>No comment yet</td>
+    @if(\Auth::User()->hasRole('department_manager'))
+        @if($task->user || $task->users->count() > 0)
+            @include('tasks._task')
         @endif
-        
-        <td>{{ $task->user->name }}</td>
-        <td>
-            @if($task->users->count() > 0)
-                @foreach($task->users as $user)
-                    {{ $user->name }} <br>
-                @endforeach
-            @endif
-        </td>
-        <td><a class="btn btn-secondary" href="/task/{{ $task->id }}">View</a></td>
-    </tr>
+    @elseif(\Auth::User()->hasRole('department_member'))
+        @if($task->user && $task->users->count() > 0)
+            @include('tasks._task')
+        @endif
+    @endif
     @endforeach
 </tbody>
 </table>
-
-
-
-@if(Auth::user()->hasRole('department_manager'))
-    You are logged in as a department manager.
-    <!-- show all public tasks( assigned to or assigned to 
-    their department) -->
-@else
-    You are logged in as a junior employee.
-    <!-- show all public tasks  -->
-@endif
