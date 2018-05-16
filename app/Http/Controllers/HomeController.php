dfeeba2e359e8b50b554bfa3fedec472cb5124cd
johnmusiu <johnmusiu@gmail.com>
2018-05-16 13:12:46 +0300

    Filter tasks lists according to access controls Return tasks in respect to whether a user is a manager or a member

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,9 +25,52 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $request->user()->authorizeRoles(['department_member', 'department_manager']);
-        $tasks = Task::where('status', '!=', 'closed')
-            ->with(['user', 'users', 'latest_progress'])
-            ->get();
-        return view('home', compact('tasks'));
+
+        $tasks_public = Task::where('access', 'public')
+            ->with(['user', 'latest_progress', 'latest_comment', 'assigned_to'])
+            ->latest()->get();
+
+        if(\Auth::User()->hasRole('department_manager')){
+            //get all private tasks created by department members
+            $tasks_private = Task::where('access', 'private')
+                ->with(['user' => function($query){
+                    $query->where('users.department_id', 
+                        \Auth::User()->department_id);
+                },
+                'assigned_to', 'latest_progress', 'latest_comment'])
+                ->latest()->get();
+            //get all private tasks assigned to department members
+            $tasks_private_as = Task::where('access', 'private')
+                ->with(['user', 'assigned_to'=> function($query){
+                    $query->where('id', \Auth::User()->id);
+                }
+                , 'latest_progress', 'latest_comment'])
+                ->latest()->get();
+                
+            $tasks_private = $tasks_private->merge($tasks_private_as);
+
+        }else if(\Auth::User()->hasRole('department_member')){
+            //private tasks a user is assigned
+            $tasks_private = Task::where('access', 'private')
+                ->with(['user', 'latest_progress', 'latest_comment', 
+                'assigned_to'=> function($query){
+                        $query->where('id', \Auth::User()->id);
+                    } 
+                ])->latest()->get();
+            //private tasks a user created
+            $tasks_private_p = Task::where('user_id', \Auth::User()->id)
+                ->with(['user', 'assigned_to', 'latest_progress', 
+                    'latest_comment'])
+                ->latest()->get();
+
+            $tasks_private = $tasks_private->merge($tasks_private_p);
+
+        }else{
+            session()->flash("error", "Access denied");
+            return back();
+        }
+        $tasks = $tasks_public->merge($tasks_private);
+
+        return view('home', compact('tasks', 'private_tasks', 'public_tasks'));
     }
 }
